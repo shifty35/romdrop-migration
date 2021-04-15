199bd640c932f31c6f845e1487be5c7ccee4fb29
Benjamin Ness <benjamin.ness@gdit.com>
2021-04-14 21:10:57 -0500

    python3 changes, loop over all user roms.  move directory configuration to .ini file, use configparser

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Rename output to input 
-#mv metadata_output metadata_input
+# Rename output to input
+#mv metadata metadata_input
 #mv patched_roms_output patched_roms_input
 
 # Fetch latest metadata
 git clone --depth 1 https://github.com/speepsio/romdrop.git
-mv romdrop/metadata metadata_output
+mv romdrop/metadata metadata
 rm -rf romdrop
 
 # Fetch latest patches
-git clone --depth 1 https://github.com/speepsio/romdrop-patches.git 
+git clone --depth 1 https://github.com/speepsio/romdrop-patches.git
+
+# Patch stock roms
+python3 patch.py
+
+# Migrate user roms
+python3 migrate.py
